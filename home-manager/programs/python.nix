eee736874700c6d0b72d20c73643977ddb729c1c
Yanni Papandreou <nepo1y@gmail.com>
2023-07-17 00:52:09 +0100

    python pyyaml module added to home env for rofi obsidian task script

--- home-manager/programs/python.nix
+++ home-manager/programs/python.nix
@@ -1,15 +1,19 @@
 { pkgs, ... }:
+let
+  my-python-packages = ps: with ps; [
+    ipython
+    jsonpickle
+    pycairo
+    pygobject3
+    requests
+    wheel
+    pyyaml
+  ];
+in
 {
 
   home.packages = with pkgs; [
-    (python311.withPackages(ps: with ps; [
-      ipython
-      jsonpickle
-      pycairo
-      pygobject3
-      requests
-      wheel
-    ]))
+    (python311.withPackages my-python-packages)
   ];
 
 }
