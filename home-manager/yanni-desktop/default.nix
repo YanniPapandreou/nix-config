af89e9bc86ded6793f00ddc6a50c414aa6024f69
Yanni Papandreou <nepo1y@gmail.com>
2023-09-16 08:45:54 +0100

    home file for desktop

---
+++ home-manager/yanni-desktop/default.nix
@@ -0,0 +1,71 @@
+{ inputs, outputs, lib, config, pkgs, ... }:
+{
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+    # inputs.hyprland.homeManagerModules.default ../hyprland
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./defaultApps.nix
+    ../programs
+    ../fonts.nix
+    ../themes.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      inputs.neovim-nightly-overlay.overlay
+      inputs.nixneovimplugins.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
+  home = {
+    username = "yanni";
+    homeDirectory = "/home/yanni";
+  };
+
+  #xdg.userDirs = {
+  #  enable = true;
+  #  createDirectories = true;
+  #};
+
+  #programs.password-store = {
+  #  enable = true;
+  #};
+
+  # Enable home-manager
+  programs.home-manager.enable = true;
+
+  #services.network-manager-applet.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
