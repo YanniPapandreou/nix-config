6c963f0677e18a6805263c95eb053a81d8e9ebae
Yanni Papandreou <nepo1y@gmail.com>
2024-04-04 18:29:17 +0100

    add cosmic flake

--- flake.nix
+++ flake.nix
@@ -13,9 +13,14 @@
     nixneovimplugins.url = "github:jooooscha/nixpkgs-vim-extra-plugins";
     # ags.url = "github:Aylur/ags";
     stylix.url = "github:danth/stylix";
+
+    nixos-cosmic = {
+      url = "github:lilyinstarlight/nixos-cosmic";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager,... }@inputs:
+  outputs = { self, nixpkgs, home-manager, nixos-cosmic,... }@inputs:
     let
       inherit (self) outputs;
       forAllSystems = nixpkgs.lib.genAttrs [
