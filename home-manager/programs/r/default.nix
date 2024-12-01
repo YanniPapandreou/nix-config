a9599acfe66c03ba99095c413721f35e52c65135
Yanni Papandreou <nepo1y@gmail.com>
2024-12-01 12:35:36 +0000

    Revert "before updating"
    This reverts commit c1f37dc4a5d4923d5ab711de13e359b9ba5a69ff.

--- home-manager/programs/r/default.nix
+++ home-manager/programs/r/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 let
   rWrapper = pkgs.rWrapper;
-  # rstudioWrapper = pkgs.rstudioWrapper;
+  rstudioWrapper = pkgs.rstudioWrapper;
   radianWrapper = pkgs.radianWrapper;
   Rpackages = with pkgs.rPackages; [
     ISLR
@@ -50,29 +50,29 @@ DBI
   ];
   R-with-my-packages = rWrapper.override{ packages = Rpackages; };
   radian-with-my-packages = radianWrapper.override{ packages = Rpackages; };
-  # RStudio-with-my-packages = rstudioWrapper.override{ packages = Rpackages; };
+  RStudio-with-my-packages = rstudioWrapper.override{ packages = Rpackages; };
 in
 {
   home.packages = [
     R-with-my-packages
     radian-with-my-packages
-    # RStudio-with-my-packages
+    RStudio-with-my-packages
     pkgs.rstyler
     pkgs.gnumake
   ];
 
-  # xdg.desktopEntries = {
-  #   RStudio = {
-  #     name = "RStudio";
-  #     comment = "R IDE";
-  #     genericName = "IDE";
-  #     exec = ''
-  #     rstudio
-  #     '';
-  #     # icon = "";
-  #     type = "Application";
-  #   };
-  # };
+  xdg.desktopEntries = {
+    RStudio = {
+      name = "RStudio";
+      comment = "R IDE";
+      genericName = "IDE";
+      exec = ''
+      rstudio
+      '';
+      # icon = "";
+      type = "Application";
+    };
+  };
 
   programs.fish.shellAliases = {
     r = "radian";
